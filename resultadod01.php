5470dbc8f08ff11806e9912e725f82ce0166c5c0
Mylena Leite Bortolozzo <69700356+mylenaleiteb@users.noreply.github.com>
2023-07-31 16:03:22 -0300

    Desafio 1
    Desafio referente à atividade de criar um formulário em que o usuário insere um número e há o retorno do número que foi inserido, seu antecessor, seu sucessor e sua raiz quadrada.

---
+++ resultadod01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="estiloDod001.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado final: </h1>
+    </header>
+    <main>
+        <?php
+            $numero = $_GET['numero'];
+
+            echo "O número digitado foi $numero"; ?> </br>
+        <?= "O seu antecessor é " . $numero -1; ?> </br>
+
+        <?= "O seu sucessor é " . $numero + 1; ?> </br>
+
+        <?= "E sua raiz quadrada é " . sqrt($numero); ?> </br>
+
+        <p> <a href="javascript:history.go(-1)"> Voltar </a> </p>
+    </main>
+</body>
+</html>
